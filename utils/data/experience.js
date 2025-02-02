f121e1c8083fa5f298a314af99ccc597b44fa64f
manish824 <manishsaitalasila2000@gmail.com>
2025-02-02 15:17:07 -0500

    Updated portfolio project

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,24 @@
 export const experiences = [
     {
       id: 1,
+      title: 'Application Developer',
+      company: "Forest River Inc",
+      duration: "(Sep 2024 – Present)"
+    },
+    {
+      id: 2,
       title: 'Software Engineer',
       company: "Cognizant Technology Solutions",
       duration: "(July 2021 – Feb 2022)"
     },
     {
-      id: 2,
+      id: 3,
       title: "Program Analyst",
       company: "Cognizant Technology Solutions",
       duration: "(Jan 2021 – May 2021)"
     },
     {
-      id: 3,
+      id: 4,
       title: "Software Developer",
       company: "Code Tantra Tech Solutions Pvt. Ltd",
       duration: "(June 2020 – Dec 2020)"
